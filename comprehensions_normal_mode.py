43e342a6754147945c92e8659f09738e6a1666e8
emilycharris <emily.harris.mail@gmail.com>
2016-05-23 17:10:18 -0400

    began adding functions

--- comprehensions_normal_mode.py
+++ comprehensions_normal_mode.py
@@ -1,29 +1,44 @@
-#Remove all vowels from this sentence List Comprehensions are the Greatest!
-
-vowels = ('a', 'e', 'i', 'o', 'u')
-sentence = "List Comprehensions are the Greatest!"
-
-result = ''.join([letter for letter in sentence if letter not in vowels])
-print(result)
-
-#============================================
-#Create a list of Water Temps for each day from the data set.
-
 import csv
 
-csvfile = open("dataset.txt", 'r')
-data = csv.reader(csvfile, delimiter=',')
-dataset = list(data)
-#print(dataset)
+#Remove all vowels from this sentence List Comprehensions are the Greatest!
 
-water_temp = [row[4] for row in dataset]
+def remove_vowels(string):
 
-print(water_temp)
+    vowels = ('a', 'e', 'i', 'o', 'u')
+    result = ''.join([letter for letter in string if letter not in vowels])
+    print(result)
+
+remove_vowels("List Comprehensions are the Greatest!")
+
+#Create a list of Water Temps for each day from the data set.
+
+
+def open_file():
+    csvfile = open("dataset.txt", 'r')
+    data = csv.reader(csvfile, delimiter=',')
+    dataset = list(data)
+    return dataset
+
+
+def list_water_temp():
+
+    water_temp = [row[4] for row in open_file()]
+    print(water_temp)
+    return water_temp
+
+list_water_temp()
+
+
+def list_date():
+    date = [row[5] for row in open_file()]
+    return date
+
+list_date()
 
 
 #Convert the Water Temps from a string to a float
 
-float_conversion = [float(temp) for temp in water_temp[1:]]
+float_conversion = [float(temp) for temp in list_water_temp()[1:]]
 print(float_conversion)
 
 #Convert the Water Temps from Celsius to Fahrenheit rounded to an int
