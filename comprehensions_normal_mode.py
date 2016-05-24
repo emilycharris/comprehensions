7adcf03cd21c8be52863e74e0dddb1ba4a15fa11
emilycharris <emily.harris.mail@gmail.com>
2016-05-24 15:52:13 -0400

    get_dow function added

--- comprehensions_normal_mode.py
+++ comprehensions_normal_mode.py
@@ -1,5 +1,7 @@
 import csv
 import statistics
+import datetime
+from collections import defaultdict
 
 #Remove all vowels from this sentence List Comprehensions are the Greatest!
 
@@ -7,9 +9,9 @@ def remove_vowels(string):
 
     vowels = ('a', 'e', 'i', 'o', 'u')
     result = ''.join([letter for letter in string if letter not in vowels])
-    print(result)
+    return result
+print("Remove Vowels: ", remove_vowels("List Comprehensions are the Greatest!"))
 
-remove_vowels("List Comprehensions are the Greatest!")
 
 #Create a list of Water Temps for each day from the data set.
 
@@ -20,20 +22,18 @@ def open_file():
     dataset = list(data)
     return dataset
 
-print(open_file())
+
 def list_water_temp():
 
     water_temp = [row[4] for row in open_file()]
     return water_temp
-
-print(list_water_temp())
+print("Water Temp List: ", list_water_temp())
 
 
 def list_date():
     date = [row[5] for row in open_file()]
     return date
-
-print(list_date())
+print("Date List: ", list_date())
 
 #Convert the Water Temps from a string to a float
 
@@ -41,8 +41,7 @@ print(list_date())
 def float_conversion_func():
     float_conversion = [float(temp) for temp in list_water_temp()[1:]]
     return(float_conversion)
-
-print(float_conversion_func())
+print("Temps as Float: ", float_conversion_func())
 
 #Convert the Water Temps from Celsius to Fahrenheit rounded to an int
 
@@ -50,8 +49,7 @@ print(float_conversion_func())
 def temp_conversion_C_to_F():
     fahrenheit = [int(temp * 1.8 + 32) for temp in float_conversion_func()]
     return fahrenheit
-
-print(temp_conversion_C_to_F())
+print("Temps Converted C to F: ", temp_conversion_C_to_F())
 
 #Create a dictionary with Date as the key and Wave Height as the value
 
@@ -64,15 +62,27 @@ def wave_height_dictionary():
     wave_height = wave_height_func()
     dictionary = {dates[i]: wave_height[i] for i in range(len(dates))}
     return dictionary
-
-print(wave_height_dictionary())
+print("Wave Height Dictionary: ", wave_height_dictionary())
 
 #Create a dictionary with the average wave height for each day
 
-#for (key, value) in wave_height_dictionary():
+def average_wave_height():
 
+    dates = []
+    for date in wave_height_dictionary():
+        dates.append(date) #==>string to list
+    dates.remove("Date")
+    return dates
+dates = average_wave_height()
+print(dates)
 
-#Create a nested comprehension to get the average of the Homework 1 grades.
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+def get_dow(date_string):
+    return days[datetime.datetime.strptime(date_string, '%Y-%m-%d').weekday()]
+
+print([get_dow(date) for date in dates])
+
 
 def homework1_avg():
     grade_dictionary = {'Gale': {'Homework 1': 88, 'Homework 2': 76},
@@ -82,6 +92,5 @@ def homework1_avg():
     homework_1 = []
     [homework_1.append(grades["Homework 1"]) for (person, grades) in grade_dictionary.items()]
 
-    print(statistics.mean(homework_1))
-
-homework1_avg()
+    return statistics.mean(homework_1)
+print("Homework 1 Average: ", homework1_avg())
